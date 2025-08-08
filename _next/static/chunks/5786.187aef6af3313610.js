dc8b394b3ae0f87a6591402434c260e6f6627e95
xmagickit <silasforrest0322@gmail.com>
2025-08-08 15:07:56 -0400

    update

---
+++ _next/static/chunks/5786.187aef6af3313610.js
@@ -0,0 +1,34 @@
+!(function () {
+  try {
+    var e =
+        "undefined" != typeof window
+          ? window
+          : "undefined" != typeof global
+          ? global
+          : "undefined" != typeof self
+          ? self
+          : {},
+      d = new e.Error().stack;
+    d &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[d] = "779ed9dd-6909-4465-981d-cf2298e2643d"),
+      (e._sentryDebugIdIdentifier =
+        "sentry-dbid-779ed9dd-6909-4465-981d-cf2298e2643d"));
+  } catch (e) {}
+})();
+("use strict");
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [5786],
+  {
+    75786: function (e, d, t) {
+      t.r(d),
+        t.d(d, {
+          default: function () {
+            return n;
+          },
+        });
+      var n =
+        'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none"><g clip-path="url(%23a)"><path fill="%230078D4" d="M0 0h22.755v22.745H0V0Zm25.245 0H48v22.745H25.245V0ZM0 25.245h22.755V48H0V25.245Zm25.245 0H48V48H25.245"/></g><defs><clipPath id="a"><path fill="%23fff" d="M0 0h48v48H0z"/></clipPath></defs></svg>';
+    },
+  },
+]);
